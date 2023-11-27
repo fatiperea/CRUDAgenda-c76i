126ae987330c092a0066df203cb27aa199abde8c
fatiperea <fatimaperea3pppp0@gmail.com>
2023-11-26 22:44:26 -0300

    se agrega admin de contacto en modal y validaciones de form

---
+++ js/app.js
@@ -0,0 +1,5 @@
+import Contacto from "./classContacto";
+
+const contacto = new Contacto(1, 'Juan', 'Pérez', 'juan.perez@email.com', '555-123-4567');
+
+console.log(contacto);
---
+++ js/classContacto.js
@@ -0,0 +1,53 @@
+export default class Contacto {
+    #id;
+    #nombre;
+    #apellido;
+    #email;
+    #celular;
+  
+    constructor(id, nombre, apellido, email, celular) {
+      this.#id = id;
+      this.#nombre = nombre;
+      this.#apellido = apellido;
+      this.#email = email;
+      this.#celular = celular;
+    }
+  
+    get id() {
+      return this.#id;
+    }
+  
+    get nombre() {
+      return this.#nombre;
+    }
+  
+    set nombre(nuevoNombre) {
+      this.#nombre = nuevoNombre;
+    }
+  
+    get apellido() {
+      return this.#apellido;
+    }
+  
+    set apellido(nuevoApellido) {
+      this.#apellido = nuevoApellido;
+    }
+  
+    get email() {
+      return this.#email;
+    }
+  
+    set email(nuevoEmail) {
+      this.#email = nuevoEmail;
+    }
+  
+    get celular() {
+      return this.#celular;
+    }
+  
+    set celular(nuevoCelular) {
+      this.#celular = nuevoCelular;
+    }
+  };
+  
+  
